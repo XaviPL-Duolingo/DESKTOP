9aa56e1c0c0eb5b7d567a13e996f8eb094f050fd
Xavi-PL <xavipl.mvk25@gmail.com>
2021-02-14 12:48:32 +0100

    + PopUpWindow Creado frame y controlador, falta por implementar Hechas pruebas con JSON

---
+++ src/main/java/com/duolingo/app/desktop/controllers/PopUpWindowController.java
@@ -0,0 +1,16 @@
+package com.duolingo.app.desktop.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PopUpWindowController implements Initializable {
+
+    private String contextPopUp;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
